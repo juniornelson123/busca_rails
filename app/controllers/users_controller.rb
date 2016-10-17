29ac0310b43523f0e93eb3e432254fcde3f71951
junior123nelson@gmail.com <junior123nelson@gmail.com>
2016-10-17 18:04:02 -0300

    base da aplicação: gem ransack, kaminari, bootstrap, simple-form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,88 @@
+require 'httparty'
+
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /users
+  # GET /users.json
+  def index
+
+   @users = User.all.order(:name).page params[:page]
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+  end
+
+  # GET /users/new
+  def new
+  
+
+    response = HTTParty.get("http://api.randomuser.me/")
+    
+    code = Random.rand(6000)
+    name = response.parsed_response['results'][0]['name']['first']
+
+    @user = User.new(name: name, code: code)
+
+
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+
+
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :code)
+    end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+
+
+400.times do |k| 
+	response = HTTParty.get("http://api.randomuser.me/")
+	puts response
+	code = Random.rand(6000)
+	name = response.parsed_response['results'][0]['name']['first']
+
+	user = User.create!(name: name, code: code)
+	puts "nome de usuario: #{user.name}, code #{user.code}"
+
+end
