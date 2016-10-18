dcb7d24280eb77bf4ad99ca7f4eb8ca09c1277b3
junior123nelson@gmail.com <junior123nelson@gmail.com>
2016-10-17 21:40:28 -0300

    estilizando autocomplete

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,12 +2,12 @@ require 'httparty'
 
 
 10.times do |k| 
-	response = HTTParty.get("http://api.randomuser.me/")
-	puts response
-	code = Random.rand(6000)
-	name = response.parsed_response['results'][0]['name']['first']
+    response = HTTParty.get("http://api.randomuser.me/")
+    puts response
+    code = Random.rand(6000)
+    name = response.parsed_response['results'][0]['name']['first']
 
-	user = User.create!(name: name, code: code)
-	puts "nome de usuario: #{user.name}, code #{user.code}"
+    user = User.create!(name: name, code: code)
+    puts "nome de usuario: #{user.name}, code #{user.code}"
 
 end
